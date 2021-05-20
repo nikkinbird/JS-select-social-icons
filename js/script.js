69acb0158ae5e7d19487db62f155e46651d06462
Nikki Bollman <nikki.bollman@gmail.com>
2021-05-19 21:11:35 -0500

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,12 @@
+var instagram = document.querySelector(".fa-instagram");
+//console.log(instagram);
+instagram.style.color = "#d45bb9";
+
+var facebook = document.querySelector(".fa-facebook");
+//console.log(facebook);
+facebook.style.color = "#6a99b5";
+
+var hours = document.querySelector(".contact p");
+//console.log(hours);
+hours.style.border = "solid 1px #d45bb9";
+hours.style.padding = "1em";
